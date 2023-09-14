a5f5eacfe1cf240e2d5c1c45524636ed721c0d62
Alejandro Mongua <alejom.tv@gmail.com>
2023-09-13 23:06:29 -0500

    Encapsulado

--- talleres/Taller3/main.py
+++ talleres/Taller3/main.py
@@ -1,53 +1,75 @@
 import arcade
-import math
 from PIL import Image
 
 class CustomSprite():
     def __init__(self, path:str, columns:int, rows:int):
+        self.__image = Image.open(path)
+        self.__image_width, self.__image_height = self.__image.size
+        self.__columns = columns
+        self.__rows = rows
+        self.__path = path
+        self.__spritesize_x = self.__image_width // columns
+        self.__spritesize_y = self.__image_height // rows
+        self.__textures = []
+        self.__setup()
 
-        image = Image.open(path)
+    def __setup(self):
+        for i in range(self.__rows):
+            for j in range(self.__columns):
+                texture = arcade.load_texture(self.__path, 
+                                              x=j*self.__spritesize_x,
+                                              y=i*self.__spritesize_y,
+                                              width=self.__spritesize_x,
+                                              height=self.__spritesize_y)
+                self.__textures.append(texture)
 
-        self.image_width, self.image_height = image.size
-
-        self.columns = columns
-        self.rows = rows
-        self.path = path
-        self.spritesize_x = self.image_width // columns
-        self.spritesize_y = self.image_height // rows
-        self.textures = []
-        self.setup()
-
-    def setup(self):
-        for i in range(self.rows):
-            for j in range(self.columns):
-                texture = arcade.load_texture(self.path, 
-                                              x=j*self.spritesize_x,
-                                              y=i*self.spritesize_y,
-                                              width=self.spritesize_x,
-                                              height=self.spritesize_y)
-                self.textures.append(texture)
+    @property
+    def textures(self):
+        return self.__textures
 
 
 class CustomAnimation(arcade.AnimatedTimeBasedSprite):
     def __init__(self, scale, custom_sprite:CustomSprite, frames:list, duration:int):
-        self.custom_sprite = custom_sprite
+        self.__custom_sprite = custom_sprite
+        super().__init__(scale=scale, 
+                         image_width=custom_sprite._CustomSprite__image_width, 
+                         image_height=custom_sprite._CustomSprite__image_height)
+        self.__frames_ = frames
+        self.__duration = duration
+        self.__setup()
 
-        super().__init__(scale=scale, image_width=custom_sprite.image_width, image_height=custom_sprite.image_height)
-
-        self.frames_ = frames
-        self.duration = duration
-
-        self.setup()
-
-
-    def setup(self):
-        for i in range(len(self.custom_sprite.textures)):
-            if i not in self.frames_:
+    def __setup(self):
+        textures = self.__custom_sprite.textures
+        for i in range(len(textures)):
+            if i not in self.__frames_:
                 continue
-            texture = self.custom_sprite.textures[i]
+            texture = textures[i]
             self.textures.append(texture)
-            frame = arcade.AnimationKeyframe(0, self.duration, texture)
+            frame = arcade.AnimationKeyframe(0, self.__duration, texture)
             self.frames.append(frame)
+
+    @property
+    def position(self):
+        return self.center_x, self.center_y
+    
+    @position.setter
+    def position(self, value):
+        self.center_x, self.center_y = value
+
+    def update_position(self, delta_x, delta_y):
+        self.center_x += delta_x
+        self.center_y += delta_y
+
+        if(self.center_x > 824):
+            self.center_x = -24
+        if(self.center_x < -24):
+            self.center_x = 823
+
+        if (self.center_y > 624):
+            self.center_y = -24
+        if (self.center_y < -24):
+            self.center_y = 623
+
     
 
 class GameWindow(arcade.Window):
@@ -72,12 +94,10 @@ class GameWindow(arcade.Window):
         self.player = CustomSprite("./ball.png", 3, 5)
         
         self.player_idle = CustomAnimation(3, self.player, [0,1,2], 100)
-        self.player_idle.center_x = 800 //2
-        self.player_idle.center_y = 600 //2
+        self.player_idle.position = (800 //2, 600 //2)
         self.player_idle.enabled = True
         self.player_roll = CustomAnimation(3, self.player, [7,8,9,10,11,12], 60)
-        self.player_roll.center_x = 800 //2
-        self.player_roll.center_y = 600 //2
+        self.player_roll.position = (800 //2, 600 //2)
         self.player_roll.enabled = False
 
         self.player_list.append(self.player_idle)
@@ -113,20 +133,11 @@ class GameWindow(arcade.Window):
                 anim.update_animation()
 
         if self.move_right:
-            self.player_idle.center_x += self.speed * delta_time
-            self.player_roll.center_x += self.speed * delta_time
+            self.player_idle.update_position(self.speed * delta_time, 0)
+            self.player_roll.update_position(self.speed * delta_time, 0)
         if self.move_left:
-            self.player_idle.center_x -= self.speed * delta_time
-            self.player_roll.center_x -= self.speed * delta_time
-
-        if(self.player_idle.center_x > 824):
-            self.player_idle.center_x = -24
-            self.player_roll.center_x = -24
-
-        if(self.player_idle.center_x < -24):
-            self.player_idle.center_x = 823
-            self.player_roll.center_x = 823
-        
+            self.player_idle.update_position(-self.speed * delta_time, 0)
+            self.player_roll.update_position(-self.speed * delta_time, 0)
 
 
 GameWindow(800,600, "Ejemplo encapsulamento")
