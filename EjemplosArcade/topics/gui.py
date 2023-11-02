691f8d03fb9f12135c03a64d2cda87063f0a9725
Alejandro Mongua <alejom.tv@gmail.com>
2023-11-01 20:50:45 -0500

    Bordered input text

--- EjemplosArcade/topics/gui.py
+++ EjemplosArcade/topics/gui.py
@@ -2,42 +2,40 @@ import arcade
 import arcade.gui
 import webbrowser
 
-PLACE_HOLDER = 'Tu nombre...'
+PLACE_HOLDER = "Tu nombre..."
 
-class ExampleGui():
+
+class ExampleGui:
     def __init__(self):
         self.custom_manager = arcade.gui.UIManager()
-        #self.custom_manager.enable()
+        # self.custom_manager.enable()
 
         self.v_box = arcade.gui.UIBoxLayout()
 
         # Creación de label text con fuente
-        ui_text_label = arcade.gui.UITextArea(text=" REGISTRO DE JUGADOR",
-                                              width=500,
-                                              height=40,
-                                              font_size=24,
-                                              font_name="Kenney Future")
-        
+        ui_text_label = arcade.gui.UITextArea(
+            text=" REGISTRO DE JUGADOR",
+            width=500,
+            height=40,
+            font_size=24,
+            font_name="Kenney Future",
+        )
+
         # Creación de label text
         self.v_box.add(ui_text_label.with_space_around(bottom=20))
         text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        self.ui_text_label = arcade.gui.UITextArea(text=text,
-                                              width=450,
-                                              height=60,
-                                              font_size=12,
-                                              font_name="Arial")
+        self.ui_text_label = arcade.gui.UITextArea(
+            text=text, width=450, height=60, font_size=12, font_name="Arial"
+        )
         self.v_box.add(self.ui_text_label.with_space_around(bottom=20))
 
         # Creación de un input text
         input_field = arcade.gui.UIInputText(
-          text_color=arcade.color.BLACK,
-          font_size=16,
-          width=300,
-          height=60,
-          text=PLACE_HOLDER)
-        
-        self.v_box.add(input_field.with_space_around(bottom=20))  
-        
+            text_color=arcade.color.BLACK, font_size=16, width=300, height=60, text=" "
+        )
+
+        self.v_box.add(input_field.with_space_around(top=10).with_border())
+
         # Creación de UIFlatButton
         ui_flatbutton = arcade.gui.UIFlatButton(text="Enviar", width=200)
         self.v_box.add(ui_flatbutton.with_space_around(bottom=20))
@@ -57,23 +55,21 @@ class ExampleGui():
             message_box = arcade.gui.UIMessageBox(
                 width=450,
                 height=300,
-                message_text=(
-                    "Esta es una alerta"
-                ),
-                buttons=["Ok", "Cancel"]
+                message_text=("Esta es una alerta"),
+                buttons=["Ok", "Cancel"],
             )
             self.custom_manager.add(message_box)
 
         self.v_box.add(ui_texture_button.with_space_around(bottom=20))
 
-
-
-        ui_resources = arcade.gui.UILabel(text="https://api.arcade.academy/en/latest/resources.html?highlight=onscreen_controls",
+        ui_resources = arcade.gui.UILabel(
+            text="https://api.arcade.academy/en/latest/resources.html?highlight=onscreen_controls",
             width=450,
             height=60,
             font_size=12,
             text_color=arcade.color.BLUE,
-            font_name="Arial")
+            font_name="Arial",
+        )
 
         ui_resources_clickable = arcade.gui.UIInteractiveWidget(
             width=450,
@@ -81,24 +77,21 @@ class ExampleGui():
         )
         ui_resources_clickable.add(
             arcade.gui.UIAnchorWidget(
-                anchor_x="center_x",
-                anchor_y="center_y",
-                child=ui_resources)
+                anchor_x="center_x", anchor_y="center_y", child=ui_resources
+            )
         )
-        
+
         @ui_resources_clickable.event("on_click")
         def on_ui_resources_click(event):
             webbrowser.open(ui_resources.text)
 
-        
         self.v_box.add(ui_resources_clickable.with_space_around(bottom=20))
 
         # Creación de widget para contener el v_box widget, permite centrado de objetos
         self.custom_manager.add(
             arcade.gui.UIAnchorWidget(
-                anchor_x="center_x",
-                anchor_y="center_y",
-                child=self.v_box)
+                anchor_x="center_x", anchor_y="center_y", child=self.v_box
+            )
         )
 
     def draw(self):
