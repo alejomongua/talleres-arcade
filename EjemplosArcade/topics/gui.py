ecaa8e302eb38aec886060a4e1dba4822a2c5c3e
Piyoju <piyoju@gmail.com>
2023-11-01 20:21:01 -0500

    Advance on GUI example option (WIP)

--- EjemplosArcade/topics/gui.py
+++ EjemplosArcade/topics/gui.py
@@ -13,26 +13,26 @@ class ExampleGui():
         self.v_box = arcade.gui.UIBoxLayout()
 
         # Creación de label text con fuente
-        ui_text_label = arcade.gui.UITextArea(text="Aquí un Título",
-                                              width=450,
+        ui_text_label = arcade.gui.UITextArea(text=" REGISTRO DE JUGADOR",
+                                              width=500,
                                               height=40,
                                               font_size=24,
                                               font_name="Kenney Future")
         
         # Creación de label text
-        self.v_box.add(ui_text_label.with_space_around(bottom=0))
+        self.v_box.add(ui_text_label.with_space_around(bottom=20))
         text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         self.ui_text_label = arcade.gui.UITextArea(text=text,
                                               width=450,
                                               height=60,
                                               font_size=12,
                                               font_name="Arial")
-        self.v_box.add(self.ui_text_label.with_space_around(bottom=0))
+        self.v_box.add(self.ui_text_label.with_space_around(bottom=20))
 
         # Creación de un input text
         input_field = arcade.gui.UIInputText(
           text_color=arcade.color.BLACK,
-          font_size=24,
+          font_size=16,
           width=300,
           height=60,
           text=PLACE_HOLDER)
@@ -55,9 +55,25 @@ class ExampleGui():
         # Click handler
         @ui_texture_button.event("on_click")
         def on_click_texture_button(event):
-            print("UITextureButton pressed", event)
+            message_box = arcade.gui.UIMessageBox(
+                width=450,
+                height=300,
+                message_text=(
+                    "Esta es una alerta"
+                ),
+                buttons=["Ok", "Cancel"]
+            )
+            self.custom_manager.add(message_box)
 
         self.v_box.add(ui_texture_button.with_space_around(bottom=20))
+
+
+        ui_resources = arcade.gui.UITextArea(text="https://api.arcade.academy/en/latest/resources.html?highlight=onscreen_controls",
+                                              width=450,
+                                              height=60,
+                                              font_size=12,
+                                              font_name="Arial")
+        self.v_box.add(ui_resources.with_space_around(bottom=20))
 
         # Creación de widget para contener el v_box widget, permite centrado de objetos
         self.custom_manager.add(
